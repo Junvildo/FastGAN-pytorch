b5b28cd2ffcd171d8a29eec66ece9ad4ba70033a
Junvildo <tienloc317@gmail.com>
2024-05-07 15:37:50 +0700

    PatchGAN training code

---
+++ train_with_patch.py
@@ -0,0 +1,214 @@
+import torch
+from torch import nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data.dataloader import DataLoader
+from torchvision import transforms
+from torchvision import utils as vutils
+from torch.optim.lr_scheduler import CosineAnnealingWarmRestarts
+
+import argparse
+import random
+from tqdm import tqdm
+
+from models import Generator
+from model_patch import weights_init, NLayerDiscriminator, PixelDiscriminator
+from operation import copy_G_params, load_params, get_dir
+from operation import ImageFolder, InfiniteSamplerWrapper
+from diffaug import DiffAugment
+import pandas as pd
+policy = 'color,translation'
+
+def train_d(netD, images, label="real"):
+    netD.zero_grad()
+    pred = netD(images)
+    if label == "real":
+        err = torch.mean((pred - 1)**2)
+    else:
+        err = torch.mean(pred**2)
+    err.backward()
+    return err.item()
+
+def train(args):
+
+    data_root = args.path
+    use_pixel = args.use_pixel
+    total_iterations = args.iter
+    checkpoint = args.ckpt
+    batch_size = args.batch_size
+    im_size = args.im_size
+    ndf = 64
+    ngf = 64
+    nz = 256
+    nlr = 0.0002
+    nbeta1 = 0.2
+    use_cuda = True
+    multi_gpu = True
+    dataloader_workers = args.workers
+    current_iteration = args.start_iter
+    save_interval = args.save_interval
+    saved_model_folder, saved_image_folder = get_dir(args)
+
+    
+    device = torch.device("cpu")
+    if use_cuda and torch.cuda.is_available():
+        device = torch.device("cuda:0")
+
+    transform_list = [
+            transforms.Resize((int(im_size),int(im_size))),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+        ]
+    trans = transforms.Compose(transform_list)
+    
+    if 'lmdb' in data_root:
+        from operation import MultiResolutionDataset
+        dataset = MultiResolutionDataset(data_root, trans, 1024)
+    else:
+        dataset = ImageFolder(root=data_root, transform=trans)
+
+   
+    dataloader = iter(DataLoader(dataset, batch_size=batch_size, shuffle=False,
+                      sampler=InfiniteSamplerWrapper(dataset), num_workers=dataloader_workers, pin_memory=True))
+    '''
+    loader = MultiEpochsDataLoader(dataset, batch_size=batch_size, 
+                               shuffle=True, num_workers=dataloader_workers, 
+                               pin_memory=True)
+    dataloader = CudaDataLoader(loader, 'cuda')
+    '''
+    
+    
+    #from model_s import Generator, Discriminator
+    netG = Generator(ngf=ngf, nz=nz, im_size=im_size)
+    netG.apply(weights_init)
+
+    if use_pixel:
+        netD = PixelDiscriminator(input_nc=3, ndf=ndf, im_size=im_size)
+    else:
+        netD = NLayerDiscriminator(input_nc=3, ndf=ndf, im_size=im_size)
+    netD.apply(weights_init)
+
+    # Freeze or No Freeze
+    # freeze_list = [netD.down_from_big, netD.down_from_small, netD.down_4, netD.down_8, netD.down_16, netD.down_32, netD.down_64]
+    # if args.freeze!=1:
+    #     print('Unfreeze the model')
+    # else:
+    #     print('Freeze the Discriminator')
+    #     for layer in freeze_list:
+    #         for param in layer.parameters():
+    #             param.requires_grad = False
+
+    netG.to(device)
+    netD.to(device)
+
+    avg_param_G = copy_G_params(netG)
+
+    fixed_noise = torch.FloatTensor(8, nz).normal_(0, 1).to(device)
+    
+    optimizerG = optim.Adam(netG.parameters(), lr=nlr, betas=(nbeta1, 0.999))
+    optimizerD = optim.Adam(netD.parameters(), lr=nlr, betas=(nbeta1, 0.999))
+
+    # Initialize the learning rate schedulers
+    schedulerG = CosineAnnealingWarmRestarts(optimizerG, T_0=1000, T_mult=1, eta_min=1e-5)
+    schedulerD = CosineAnnealingWarmRestarts(optimizerD, T_0=1000, T_mult=1, eta_min=1e-5)
+
+
+    if checkpoint != 'None':
+        ckpt = torch.load(checkpoint)
+        netG.load_state_dict({k.replace('module.', ''): v for k, v in ckpt['g'].items()})
+        netD.load_state_dict({k.replace('module.', ''): v for k, v in ckpt['d'].items()})
+        avg_param_G = ckpt['g_ema']
+        optimizerG.load_state_dict(ckpt['opt_g'])
+        optimizerD.load_state_dict(ckpt['opt_d'])
+        current_iteration = int(checkpoint.split('_')[-1].split('.')[0])
+        del ckpt
+        
+    if multi_gpu:
+        netG = nn.DataParallel(netG.to(device))
+        netD = nn.DataParallel(netD.to(device))
+    
+    loss_d = []
+    loss_g = []
+
+    for iteration in tqdm(range(current_iteration, total_iterations+1)):
+        real_image = next(dataloader)
+        real_image = real_image.to(device)
+        current_batch_size = real_image.size(0)
+        noise = torch.Tensor(current_batch_size, nz).normal_(0, 1).to(device)
+
+        fake_images = netG(noise)
+
+        real_image = DiffAugment(real_image, policy=policy)
+        fake_images = [DiffAugment(fake, policy=policy) for fake in fake_images]
+        
+        ## 2. train Discriminator
+        netD.zero_grad()
+
+        err_dr = train_d(netD, real_image, label="real")
+        train_d(netD, [fi.detach() for fi in fake_images], label="fake")
+        optimizerD.step()
+        schedulerD.step()  # Step the scheduler for the discriminator
+
+        ## 3. train Generator
+        netG.zero_grad()
+        pred_g = netD(fake_images)
+        err_g = torch.mean((pred_g - 1)**2)
+
+        err_g.backward()
+        optimizerG.step()
+        schedulerG.step()  # Step the scheduler for the generator
+
+        # Save the loss
+        loss_d.append(err_dr)
+        loss_g.append(-err_g.item())
+
+        for p, avg_p in zip(netG.parameters(), avg_param_G):
+            avg_p.mul_(0.999).add_(0.001 * p.data)
+
+        if iteration % 1000 == 0:
+            print("GAN: loss d: %.5f    loss g: %.5f"%(err_dr, -err_g.item()))
+          
+        if iteration % (save_interval*50) == 0:
+            backup_para = copy_G_params(netG)
+            load_params(netG, avg_param_G)
+            with torch.no_grad():
+                vutils.save_image(netG(fixed_noise)[0].add(1).mul(0.5), saved_image_folder+'/%d.jpg'%iteration, nrow=4)
+                vutils.save_image(F.interpolate(real_image, 128).add(1).mul(0.5), saved_image_folder+'/rec_%d.jpg'%iteration )
+            load_params(netG, backup_para)
+
+        if iteration % (save_interval*100) == 0 or iteration == total_iterations:
+            backup_para = copy_G_params(netG)
+            load_params(netG, avg_param_G)
+            torch.save({'g':netG.state_dict(),'d':netD.state_dict()}, saved_model_folder+'/%d.pth'%iteration)
+            load_params(netG, backup_para)
+            torch.save({'g':netG.state_dict(),
+                        'd':netD.state_dict(),
+                        'g_ema': avg_param_G,
+                        'opt_g': optimizerG.state_dict(),
+                        'opt_d': optimizerD.state_dict()}, saved_model_folder+'/all_%d.pth'%iteration)
+    loss_data = {'epoch': range(current_iteration, total_iterations+1), 'D_loss': loss_d, 'G_loss': loss_g}
+    loss_df = pd.DataFrame(data=loss_data)
+    loss_df.to_csv('loss.csv')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='region gan')
+
+    parser.add_argument('--path', type=str, default='../lmdbs/art_landscape_1k', help='path of resource dataset, should be a folder that has one or many sub image folders inside')
+    parser.add_argument('--output_path', type=str, default='./', help='Output path for the train results')
+    parser.add_argument('--cuda', type=int, default=0, help='index of gpu to use')
+    parser.add_argument('--name', type=str, default='test1', help='experiment name')
+    parser.add_argument('--iter', type=int, default=50000, help='number of iterations')
+    parser.add_argument('--start_iter', type=int, default=0, help='the iteration to start training')
+    parser.add_argument('--batch_size', type=int, default=8, help='mini batch number of images')
+    parser.add_argument('--im_size', type=int, default=1024, help='image resolution')
+    parser.add_argument('--ckpt', type=str, default='None', help='checkpoint weight path if have one')
+    parser.add_argument('--workers', type=int, default=2, help='number of workers for dataloader')
+    parser.add_argument('--save_interval', type=int, default=100, help='number of iterations to save model')
+    parser.add_argument('--freeze', type=int, default=0, help='to freeze pretrained model params or not, 0=No Freeze, 1=Freeze')
+    parser.add_argument('--use_pixel', type=int, default=0, help='to use pixel PatchGAN or NLayersPatchGAN, 1=Pixel PatchGAN, 0=NLayersPatchGAN')
+
+    args = parser.parse_args()
+    print(args)
+
+    train(args)
