ab6dd4d5324243e0f09741f9fa009f3a92b890d8
Trần Nguyễn Tiến Lộc <48644569+Junvildo@users.noreply.github.com>
2024-06-01 11:25:59 +0700

    Update train.py

--- train.py
+++ train.py
@@ -64,7 +64,7 @@ def train(args):
     nz = 256
     nlr = 0.0002
     nbeta1 = 0.5
-    use_cuda = False
+    use_cuda = True
     multi_gpu = True
     dataloader_workers = args.workers
     current_iteration = args.start_iter
